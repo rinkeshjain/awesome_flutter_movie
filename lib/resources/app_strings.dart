6e624b2904f9316673cfcea3f8a7c4fc1897b55f
rinkesh <rinkesh.jain@davintafinserv.com>
2023-03-19 22:23:13 +0530

    string and Color file added

---
+++ lib/resources/app_strings.dart
@@ -0,0 +1,9 @@
+class AppStrings {
+  static const String appTitle = 'Awesome Movies App';
+  static const String movies = 'Movies';
+  static const String shows = 'Shows';
+  static const String search = 'Search';
+  static const String watchlist = 'Watchlist';
+  static const String popularShows = 'Popular shows';
+
+}
